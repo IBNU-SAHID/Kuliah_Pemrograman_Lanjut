196ccf29a1295a87860a2a6b86fcbebe754f2f2d
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 15:18:16 +0700

    Menampilkan "Welcome to Java"

---
+++ SoloLearn/src/BasicConcepts/WelcomeToJava/Main.java
@@ -0,0 +1,8 @@
+package BasicConcepts.WelcomeToJava;
+
+public class Main {
+    public static void main(String[] args) {
+        //print "Welcome to Java."
+        System.out.println("Welcome to Java.");
+    }
+}
