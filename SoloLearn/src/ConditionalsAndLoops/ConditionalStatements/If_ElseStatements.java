9f943727547217e9173131ee9c7a3b86aff79cc6
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 20:33:45 +0700

    Belajar Conditional Statement

---
+++ SoloLearn/src/ConditionalsAndLoops/ConditionalStatements/If_ElseStatements.java
@@ -0,0 +1,13 @@
+package ConditionalsAndLoops.ConditionalStatements;
+
+public class If_ElseStatements {
+    public static void main(String[] args) {
+        int x = 10;  int y = 5;
+        if
+        ( x > y ) {
+            System.out.println(x);
+        } else {
+            System.out.println(y);
+        }
+    }
+}
