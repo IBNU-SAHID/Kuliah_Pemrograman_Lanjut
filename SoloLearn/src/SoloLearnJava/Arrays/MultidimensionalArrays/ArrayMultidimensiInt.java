1b5ee6e1f6b6f051ba25a7aa3d4f7b3c8c48e04f
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 22:44:28 +0700

    mencari tau output integer dari array multidimensi

---
+++ SoloLearn/src/SoloLearnJava/Arrays/MultidimensionalArrays/ArrayMultidimensiInt.java
@@ -0,0 +1,10 @@
+package SoloLearnJava.Arrays.MultidimensionalArrays;
+
+public class ArrayMultidimensiInt {
+    public static void main(String[] args) {
+       // What is the output of this code?
+        int array[ ][ ] =  {{3, 5, 8},
+                            {7, 54, 1, 12, 4}};
+        System.out.println(array[0][2]);
+    }
+}
