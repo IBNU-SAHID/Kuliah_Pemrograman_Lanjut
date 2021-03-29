bc2ef1363f5d2d4779b9ab86c76383aeb84eeb5d
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 22:22:47 +0700

    mengubah nilai awal variabel sum menjadi 0

--- SoloLearn/src/Arrays/SummingElementsInArrays/Sum.java
+++ SoloLearn/src/Arrays/SummingElementsInArrays/Sum.java
@@ -6,7 +6,7 @@ public class Sum {
         Fill in the blanks to calculate the sum of myArray's elements using the for loop,
         and print it to the screen. myArray is an array of doubles, containing 4 elements.
          */
-        double sum = 4.0;
+        double sum = 0.0;
         double myArray[] = {7,2,9,4,10,6};
 
         for (int x = 0; x < 4; x++) {
