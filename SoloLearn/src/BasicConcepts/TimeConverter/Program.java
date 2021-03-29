5fc0c3f5aeeb82190762d21dcc24aadd779adc0f
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 21:46:31 +0700

    Membuat program time converter

--- SoloLearn/src/BasicConcepts/TimeConverter/Program.java
+++ SoloLearn/src/BasicConcepts/TimeConverter/Program.java
@@ -2,7 +2,7 @@ package BasicConcepts.TimeConverter;
 
 import java.util.Scanner;
 
-public class program {
+public class Program {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int days = scanner.nextInt();
