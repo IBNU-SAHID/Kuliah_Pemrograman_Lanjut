88183353202830df0e6643ed67ab009d7a34c890
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 19:55:16 +0700

    Belajar operator melakukan increment

--- SoloLearn/src/BasicConcepts/IncrementAndDecrement/Increment.java
+++ SoloLearn/src/BasicConcepts/IncrementAndDecrement/Increment.java
@@ -3,6 +3,7 @@ package BasicConcepts.IncrementAndDecrement;
 public class Increment {
     public static void main(String[] args) {
         int a = 10;
+        //increment
         ++a;
         System.out.println(a);
     }
