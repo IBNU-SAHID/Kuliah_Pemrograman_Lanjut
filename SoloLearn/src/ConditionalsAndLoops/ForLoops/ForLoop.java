1351046fda56419d9a96b707a42bb57884437eb8
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 21:25:35 +0700

    Belajar Menggunakan for loop

---
+++ SoloLearn/src/ConditionalsAndLoops/ForLoops/ForLoop.java
@@ -0,0 +1,10 @@
+package ConditionalsAndLoops.ForLoops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        //How many times will the following loop run?
+        for (int i = 2; i < 10; i = i*i) {
+            System.out.println(i);
+        }
+    }
+}
