8a0e2f62d046a4039e33df520eaeecebaa65614a
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 20:20:12 +0700

    Membuat program konversi waktu

---
+++ SoloLearn/src/BasicConcepts/Project1/TimeConverter.java
@@ -0,0 +1,19 @@
+package BasicConcepts.Project1;
+
+import java.util.Scanner;
+
+public class TimeConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int days = scanner.nextInt();
+
+        //your code goes here
+        int jam = 24,menit = 60,detik=60;
+
+        jam *= days;
+        menit *= jam;
+        detik *= menit;
+
+        System.out.println(detik);
+    }
+}
