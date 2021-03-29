03ff3daec12d0e624709b054139131ac612a08a1
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 20:01:06 +0700

    Membuat Assignment Operator

---
+++ SoloLearn/src/BasicConcepts/IncrementAndDecrement/AssignmentOperator.java
@@ -0,0 +1,11 @@
+package BasicConcepts.IncrementAndDecrement;
+
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        int x = 25;
+        int y;
+        y =  x - 12;
+
+        System.out.println(y);
+    }
+}
