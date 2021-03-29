cb7152287ab5c570e970f5ac87325f79e2b500b3
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 20:12:05 +0700

    Mengerjakan quiz module 1

---
+++ SoloLearn/src/BasicConcepts/Module1Quiz/Quiz2.java
@@ -0,0 +1,9 @@
+package BasicConcepts.Module1Quiz;
+
+public class Quiz2 {
+    public static void main(String[] args) {
+        String name;
+        name = "David";
+        System.out.println(name);
+    }
+}
