0611f75de80a6e0af6b50b06df0873dabc432179
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 19:58:07 +0700

    Belajar operator melakukan Decrement

--- SoloLearn/src/BasicConcepts/IncrementAndDecrement/Decrement.java
+++ SoloLearn/src/BasicConcepts/IncrementAndDecrement/Decrement.java
@@ -3,6 +3,7 @@ package BasicConcepts.IncrementAndDecrement;
 public class Decrement {
     public static void main(String[] args) {
         int a = 10;
+        //decrement
         --a;
         System.out.println(a);
     }
