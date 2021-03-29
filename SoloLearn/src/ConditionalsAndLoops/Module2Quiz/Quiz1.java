374a78dfc2cf03ed3c6bad97a947cfd73ea8e259
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 21:39:27 +0700

    Mengerjakan Quiz module 2

---
+++ SoloLearn/src/ConditionalsAndLoops/Module2Quiz/Quiz1.java
@@ -0,0 +1,13 @@
+package ConditionalsAndLoops.Module2Quiz;
+
+public class Quiz1 {
+    public static void main(String[] args) {
+        //Fill in the blanks to print "in a loop" 7 times, using the while loop.
+        int x = 1;
+
+        while (x <= 7) {
+            System.out.println("in a loop");
+            x++;
+        }
+    }
+}
