1a4ab746bba916235a7fd8b9040338620e64f273
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 15:43:24 +0700

    Belajar menggunakan variabel

---
+++ SoloLearn/src/BasicConcepts/Variables/MyClass.java
@@ -0,0 +1,14 @@
+package BasicConcepts.Variables;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        String name ="David";
+        int age = 42;
+        double score =15.9;
+        char group = 'Z';
+        System.out.println("Name\t: " + name);
+        System.out.println("Age\t\t: " + age);
+        System.out.println("Score\t: " + score);
+        System.out.println("Group\t: " + group);
+    }
+}
