3c278ad82ffde26f3c94df063254325f9bb9aa8c
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-29 20:05:10 +0700

    Belajar String

---
+++ SoloLearn/src/BasicConcepts/Strings/hallo.java
@@ -0,0 +1,9 @@
+package BasicConcepts.Strings;
+
+public class hallo {
+    public static void main(String[] args) {
+        String var;
+        var = "Hello";
+        System.out.println(var);
+    }
+}
