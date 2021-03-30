43166ba8a391d558578c191a192f6b9ec52e261b
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 12:53:33 +0700

    menambahkan kelas B dan membuat objek baru

--- SoloLearn/src/SoloLearnJava/ClassesAndObjects/CreatingClassesAndObjects/A.java
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/CreatingClassesAndObjects/A.java
@@ -4,4 +4,11 @@ public class A {
     public void test() {
         System.out.println("Hi");
     }
+    static class B {
+        public static void main(String[] args) {
+            A obj = new A();
+            obj.test();
+        }
+
+    }
 }
