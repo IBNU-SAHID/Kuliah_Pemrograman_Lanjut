7d88dd61c1dccd7c290d2d6c4c406fc724920cbf
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 19:20:54 +0700

    Belajar import package

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/Packages/MyClass.java
@@ -0,0 +1,10 @@
+package SoloLearnJava.ClassesAndObjects.Packages;
+
+import SoloLearnJava.ClassesAndObjects.Static.Vehicle;
+
+public class MyClass {
+    public static void main(String[ ] args) {
+        Vehicle v1 = new Vehicle();
+        v1.horn();
+    }
+}
