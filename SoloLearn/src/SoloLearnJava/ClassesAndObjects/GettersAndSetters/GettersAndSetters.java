67957f480c4ff433002250c78039ddd3c524f424
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 13:27:44 +0700

    membuat getterx dan settersx

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/GettersAndSetters/GettersAndSetters.java
@@ -0,0 +1,13 @@
+package SoloLearnJava.ClassesAndObjects.GettersAndSetters;
+
+class A {
+    private int x;
+    public int getX() {
+        return x ;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+
+
+}
