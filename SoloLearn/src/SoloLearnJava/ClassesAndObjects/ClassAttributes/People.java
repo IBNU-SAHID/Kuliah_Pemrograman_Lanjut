8f99a0020095c8f8c6fa5fa065bbc88c85907129
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 13:14:52 +0700

    membuat 2 objek dari class People

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/ClassAttributes/People.java
@@ -0,0 +1,13 @@
+package SoloLearnJava.ClassesAndObjects.ClassAttributes;
+
+    //Fill in the blanks to create two objects from the class "People".
+
+public class People{
+
+    public static void main(String[] args) {
+        People obj1 = new People();
+
+        People obj2 = new People();
+    }
+
+}
