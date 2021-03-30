19af8114ad16c8c2fab690242db44386e020e849
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 17:35:02 +0700

    membuat class person

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/ValueAndReferenceTypes/Person.java
@@ -0,0 +1,12 @@
+package SoloLearnJava.ClassesAndObjects.ValueAndReferenceTypes;
+
+public class Person {
+    private int age;
+
+    public void setAge(int myage) {
+        age = myage;
+    }
+    public int getAge(){
+        return age;
+    }
+}
