fc84fc6786d794d29524b7479b2fea5ff713c686
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 10:05:33 +0700

    Mengenal Method dengan parameter

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/Methods/MethodParameters.java
@@ -0,0 +1,11 @@
+package SoloLearnJava.ClassesAndObjects.Methods;
+
+public class MethodParameters {
+    //What output results from this code?
+    public static void main(String[ ] args) {
+        doSomething(4);
+    }
+    static void doSomething(int x) {
+        System.out.println(x*x);
+    }
+}
