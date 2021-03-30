beda4e3e543d0e68efd1a15f4d86f30c2f271b54
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 10:04:05 +0700

    Mengenal Method

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/Methods/Method.java
@@ -0,0 +1,12 @@
+package SoloLearnJava.ClassesAndObjects.Methods;
+
+public class Method {
+    //Fill in the blanks to call the method "hello" from main:
+    public static void main(String[ ] args) {
+        hello();
+    }
+
+    static void hello() {
+        System.out.println("hi");
+    }
+}
