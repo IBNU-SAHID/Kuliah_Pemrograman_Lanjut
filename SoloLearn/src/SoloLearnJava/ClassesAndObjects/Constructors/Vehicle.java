8ceb57eb228ef7e5770cd6ac0613e87a3032020f
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 16:42:41 +0700

    Belajar mengenai constructor

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/Constructors/Vehicle.java
@@ -0,0 +1,11 @@
+package SoloLearnJava.ClassesAndObjects.Constructors;
+
+public class Vehicle {
+    private String color;
+    Vehicle() {
+        color = "Red";
+    }
+    Vehicle(String c) {
+        color = c;
+    }
+}
