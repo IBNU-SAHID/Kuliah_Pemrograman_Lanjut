12b357d388b03adde665eaa9db7a25c2f01eb5ad
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 13:09:04 +0700

    belajar membuat atribut

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/ClassAttributes/DefiningAttributes.java
@@ -0,0 +1,15 @@
+package SoloLearnJava.ClassesAndObjects.ClassAttributes;
+
+    /*
+    Drag and drop from the options below to define a
+    class with these attributes: age of type integer,
+    height as a double, and name as a string.
+     */
+
+class Person {
+    int age;
+    double height;
+    String name;
+
+}
+
