bc88b53edc6e352d545ff4cbc239566843a43043
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 17:25:34 +0700

    mencari tau output dari prorgam ini

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/ValueAndReferenceTypes/ValueTypes.java
@@ -0,0 +1,13 @@
+package SoloLearnJava.ClassesAndObjects.ValueAndReferenceTypes;
+
+public class ValueTypes {
+    //What is the output of this code?
+    public static void main(String[ ] args) {
+        int x = 4;
+        square(x);
+        System.out.println(x);
+    }
+    static void square(int x) {
+        x = x*x;
+    }
+}
