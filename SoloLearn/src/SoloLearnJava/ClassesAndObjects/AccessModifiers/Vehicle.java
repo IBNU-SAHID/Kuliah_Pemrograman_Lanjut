f2bc7db9a0b0df54d80a12659b2113ae7f71d61e
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 13:18:18 +0700

    Mengenal Acces Modifiers

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/AccessModifiers/Vehicle.java
@@ -0,0 +1,13 @@
+package SoloLearnJava.ClassesAndObjects.AccessModifiers;
+
+public class Vehicle {
+    private int maxSpeed;
+    private int wheels;
+    private String color;
+    private double fuelCapacity;
+
+    public void horn() {
+        System.out.println("Beep!");
+    }
+
+}
