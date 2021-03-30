7e01817aa7ea85ef2498aa0c257c927536d0ce9d
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 13:09:33 +0700

    belajar membuat class, method dan artibut

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/ClassAttributes/Vehicle.java
@@ -0,0 +1,14 @@
+package SoloLearnJava.ClassesAndObjects.ClassAttributes;
+
+//Let's create a class called Vehicle, with its corresponding attributes and methods.
+public class Vehicle {
+    int maxSpeed;
+    int wheels;
+    String color;
+    double fuelCapacity;
+    void horn() {
+        System.out.println("Beep!");
+    }
+    /*maxSpeed, wheels, color and fuelCapacity are the
+     attributes of our Vehicle class, and horn() is the only method.*/
+}
