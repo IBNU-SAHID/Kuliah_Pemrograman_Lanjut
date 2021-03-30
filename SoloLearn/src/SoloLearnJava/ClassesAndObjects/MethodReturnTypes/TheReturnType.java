d0a4b38fb390d4c23f3b85aad1b91ab02f77a328
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-03-30 11:58:58 +0700

    membuat method return type

---
+++ SoloLearn/src/SoloLearnJava/ClassesAndObjects/MethodReturnTypes/TheReturnType.java
@@ -0,0 +1,12 @@
+package SoloLearnJava.ClassesAndObjects.MethodReturnTypes;
+
+public class TheReturnType {
+    //Fill in the blanks to declare an integer, and pass it as a parameter to the test() method.
+    public static void main(String[ ] args) {
+        int x = 10;
+        test(x);
+    }
+    static void test(int x) {
+        System.out.println(x);
+    }
+}
