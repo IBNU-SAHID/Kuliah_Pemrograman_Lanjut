61f1a6742c76f7be35b4b38bce1585be6da38a9e
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-04-02 22:27:24 +0700

    Belajar tentang Threads

--- SoloLearn/src/SoloLearnJava/ExeptionsListsThreadsAndFiles/Threads/App.java
+++ SoloLearn/src/SoloLearnJava/ExeptionsListsThreadsAndFiles/Threads/App.java
@@ -2,7 +2,6 @@ package SoloLearnJava.ExeptionsListsThreadsAndFiles.Threads;
 
 //Drag and drop from the options below to implement the Runnable interface and run a new thread.
 class A implements Runnable {
-
     public void run() {
         System.out.println("Bye");
     }
@@ -12,7 +11,5 @@ public class App {
     public static void main(String[] args) {
         Thread ob = new Thread(new A());
         ob.start();
-
     }
-
 }
