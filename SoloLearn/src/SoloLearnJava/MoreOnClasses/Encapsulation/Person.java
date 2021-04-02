6aecdf01bd0ccfc6aaf2dee141d1f0d7039a1450
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-04-02 09:33:57 +0700

    mengenal encapsulation

---
+++ SoloLearn/src/SoloLearnJava/MoreOnClasses/Encapsulation/Person.java
@@ -0,0 +1,14 @@
+package SoloLearnJava.MoreOnClasses.Encapsulation;
+
+public class Person {
+    //Drag and drop from the options below to create a valid Java code with encapsulation.
+    private  int age;
+    public void setAge(int age){
+        if (age > 0) {
+
+            this.age = age;
+
+        }
+    }
+
+}
