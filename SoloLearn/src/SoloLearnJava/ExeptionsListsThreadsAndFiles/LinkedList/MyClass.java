bc9aa759f5cab527847874c95077fb36d3b71764
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-04-02 23:02:28 +0700

    Belajar LinkedList

---
+++ SoloLearn/src/SoloLearnJava/ExeptionsListsThreadsAndFiles/LinkedList/MyClass.java
@@ -0,0 +1,10 @@
+package SoloLearnJava.ExeptionsListsThreadsAndFiles.LinkedList;
+
+import java.util.LinkedList;
+public class MyClass {
+    public static void main(String[] args) {
+        LinkedList <String> list = new LinkedList<String>();
+        list.add("hallo");
+        System.out.println(list.get(0));
+    }
+}
