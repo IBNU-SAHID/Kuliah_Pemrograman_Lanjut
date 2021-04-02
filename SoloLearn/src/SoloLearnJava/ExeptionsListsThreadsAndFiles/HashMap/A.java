b06042a5ef9ff1fa19577f2b75d3ef630c0dc512
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-04-02 23:04:09 +0700

    Belajar HashMap

--- SoloLearn/src/SoloLearnJava/ExeptionsListsThreadsAndFiles/HashMap/A.java
+++ SoloLearn/src/SoloLearnJava/ExeptionsListsThreadsAndFiles/HashMap/A.java
@@ -2,6 +2,7 @@ package SoloLearnJava.ExeptionsListsThreadsAndFiles.HashMap;
 
 import java.util.HashMap;
 public class A {
+    //What is the output of this code?
     public static void main(String[ ] args) {
         HashMap<String, String> m = new HashMap<String, String>();
         m.put("A", "First");
