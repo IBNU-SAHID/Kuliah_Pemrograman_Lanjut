7d40c44c7c7d29fa7acede86529570b1151fe462
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-04-02 22:46:35 +0700

    Belajar Array list

---
+++ SoloLearn/src/SoloLearnJava/ExeptionsListsThreadsAndFiles/ArrayList/MyClass.java
@@ -0,0 +1,14 @@
+package SoloLearnJava.ExeptionsListsThreadsAndFiles.ArrayList;
+
+import java.util.ArrayList;
+public class MyClass {
+    public static void main(String[] args) {
+        //What is the output of this code?
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        System.out.println(list.get(1));
+    }
+
+}
