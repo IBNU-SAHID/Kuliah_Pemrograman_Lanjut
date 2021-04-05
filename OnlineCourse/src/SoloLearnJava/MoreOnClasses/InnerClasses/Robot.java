40b31c1cf35341c1396ee140f144efefb66369b9
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-04-05 12:19:47 +0700

    SoloLearn

--- OnlineCourse/src/SoloLearnJava/MoreOnClasses/InnerClasses/Robot.java
+++ OnlineCourse/src/SoloLearnJava/MoreOnClasses/InnerClasses/Robot.java
@@ -1,15 +1,17 @@
 package SoloLearnJava.MoreOnClasses.InnerClasses;
-
+/*
 class Robot {
     int id;
     Robot(int i){
         id = i;
-        Brain b = new Brain;
+        Brain b = new Brain();
         b.think();
     }
 }
-private class Brain{
-    public void think(){
+ class Brain {
+     public void think(){
         System.out.println(id + " is thinking");
     }
 }
+*/
+
