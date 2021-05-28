6ede35a460453f0c6ef660e3acf4f15c87be2c8a
Ibnu Sahid <12050110489@students.uin-suska.ac.id>
2021-05-28 20:25:28 +0700

    menambahkan jumlah prodi yang bisa di cari berdasarkan fakultas

--- TugasKuliah/src/NimMahasiswa/NIM.java
+++ TugasKuliah/src/NimMahasiswa/NIM.java
@@ -69,20 +69,126 @@ public class NIM {
         char kodeProdi2 = nim.charAt(5);
 
         String kodeProdi = "" + kodeProdi1 +kodeProdi2;
+        //tarbiah keguruan
 
-        if (kodeProdi.equals("01") ){
-            return "Teknik Informatika";
-        }else if (kodeProdi.equals("02") ){
-            return "Teknik Industri";
-        }else if (kodeProdi.equals("03") ){
-            return "Sistem Informasi";
-        }else if (kodeProdi.equals("04") ){
-            return "Matematika";
-        }else if (kodeProdi.equals("05") ){
-            return "Teknik Elektro";
-        }else{
-            return "Tidak Diketahui";
+        if (Fakultas().equals("TARBIYAH DAN KEGURUAN")){
+            if (kodeProdi.equals("01") ){
+                return "Hukum Keluarga (Ahwal Al-Syakhsiyah)";
+            }else if (kodeProdi.equals("02") ){
+                return "Pendidikan Bahasa Arab";
+            }else if (kodeProdi.equals("03") ){
+                return "Manajemen Pendidikan Islam";
+            }else if (kodeProdi.equals("04") ){
+                return "Pendidikan Bahasa Inggris";
+            }else if (kodeProdi.equals("05") ){
+                return "Pendidikan Matematika";
+            }else if (kodeProdi.equals("06") ){
+                return "Pendidikan Ekonomi";
+            }else if (kodeProdi.equals("07") ){
+                return "Pendidikan Kimia";
+            }else if (kodeProdi.equals("08") ){
+                return "Pendidikan Guru Madrasah Ibtidaiyah";
+            }else if (kodeProdi.equals("09") ){
+                return "Pendidikan Guru Raudhatul Athfal";
+            }else{
+                return "Tidak Diketahui";
+            }
+
+            //Syariah dan hukum
+        }else if (Fakultas().equals("SYARI’AH DAN HUKUM")){
+            if (kodeProdi.equals("01") ){
+                return "Hukum Keluarga (Ahwal Al-Syakhsiyah)";
+            }else if (kodeProdi.equals("02") ){
+                return "Hukum Ekonomi Syariah (Muamalah)";
+            }else if (kodeProdi.equals("03") ){
+                return "Perbandingan Mazhab dan Hukum";
+            }else if (kodeProdi.equals("04") ){
+                return "Hukum Tata Negara(Siyasah)";
+            }else if (kodeProdi.equals("05") ){
+                return "Ekonomi Islam";
+            }else if (kodeProdi.equals("06") ){
+                return "Perbankan Syari’ah";
+            }else if (kodeProdi.equals("07") ){
+                return "Ilmu Hukum";
+            }else{
+                return "Tidak Diketahui";
+            }
+
+            // Ushuluddin
+        }else if (Fakultas().equals("USHULUDDIN")) {
+            if (kodeProdi.equals("01")) {
+                return "Ilmu Aqidah";
+            } else if (kodeProdi.equals("02")) {
+                return "Ilmu Al-qur’an dan Tafsir";
+            } else if (kodeProdi.equals("03")) {
+                return "Perbandingan Agama";
+            } else {
+                return "Tidak Diketahui";
+            }
+
+            //Dakwah dan Ilmu Komunikasi
+        }else if (Fakultas().equals("DAKWAH & KOMUNIKASI")) {
+            if (kodeProdi.equals("01")) {
+                return "Pengembangan Masyarakat Islam";
+            } else if (kodeProdi.equals("02")) {
+                return "Bimbingan Penyuluhan Islam";
+            } else if (kodeProdi.equals("03")) {
+                return "Ilmu Komunikasi";
+            } else if (kodeProdi.equals("04")) {
+                return "Manajemen Dakwah";
+            } else {
+                return "Tidak Diketahui";
+            }
+
+            //Sains dan Teknologi
+        }else if (Fakultas().equals("SAINS DAN TEKNOLOGI")) {
+            if (kodeProdi.equals("01") ){
+                return "Teknik Informatika";
+            }else if (kodeProdi.equals("02") ){
+                return "Teknik Industri";
+            }else if (kodeProdi.equals("03") ){
+                return "Sistem Informasi";
+            }else if (kodeProdi.equals("04") ){
+                return "Matematika";
+            }else if (kodeProdi.equals("05") ){
+                return "Teknik Elektro";
+            }else{
+                return "Tidak Diketahui";
+            }
+        }else if (Fakultas().equals("PSIKOLOGI")) {
+            if (kodeProdi.equals("01") ){
+                return "Psikologi";
+            }else{
+                return "Tidak Diketahui";
+            }
+        }else if (Fakultas().equals("EKONOMI DAN ILMU SOSIAL")) {
+            if (kodeProdi.equals("01") ){
+                return "Manajemen";
+            }else if (kodeProdi.equals("02") ){
+                return "Manajemen Perusahaan";
+            }else if (kodeProdi.equals("03") ){
+                return "Akuntansi";
+            }else if (kodeProdi.equals("04") ){
+                return "Akuntansi D3";
+            }else if (kodeProdi.equals("05") ){
+                return "Ilmu Administrasi Negara";
+            }else if (kodeProdi.equals("06") ){
+                return "Administrasi Perpajakan";
+            }else{
+                return "Tidak Diketahui";
+            }
+        }else if (Fakultas().equals("PERTANIAN DAN PETERNAKAN")) {
+            if (kodeProdi.equals("01") ){
+                return "Peternakan";
+            }else if (kodeProdi.equals("02") ){
+                return "Agroteknologi";
+            }else{
+                return "Tidak Diketahui";
+            }
+        }else {
+            return "tidak diketahui";
         }
+
 
     }
 
